980831dfe56bd02b5566f661147dab8ca2db2b3d
drakcoder <sairamgundala2000@gmail.com>
2022-03-16 16:10:00 +0530

    added service functions

--- src/app/services/register-business.service.ts
+++ src/app/services/register-business.service.ts
@@ -1,9 +1,37 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { map } from 'rxjs/operators'
 
 @Injectable({
   providedIn: 'root'
 })
 export class RegisterBusinessService {
 
-  constructor() { }
+  constructor(private db: AngularFirestore) { };
+  private basePath = '/images';
+
+  createProvider(provider: Object) {
+    const providerData = JSON.parse(JSON.stringify(provider));
+    return this.db.collection('Provider').add(providerData);
+  }
+
+  getAllProviders(): Observable<any> {
+    const Providers = this.db.collection<Object>('Provider').snapshotChanges().pipe(map(actions => {
+      return actions.map(c => ({
+        providerId: c.payload.doc.id,
+        ...c.payload.doc.data() as Object
+      }));
+    }));
+    return Providers;
+  }
+
+  getProviderById(id: string): Observable<any> {
+    const Provider = this.db.doc<Object>('Provider/' + id).valueChanges();
+    return Provider;
+  }
+
+  deleteProvider(providerId: string) {
+    return this.db.doc('Provider/' + providerId).delete();
+  }
 }
