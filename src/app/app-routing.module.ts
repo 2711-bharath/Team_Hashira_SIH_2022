33130ecfea2a59c24bf696e3147b4d9aa7c4bb0e
Harshita <harshitachundru@gmail.com>
2022-03-17 03:41:31 +0530

    Home Page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,7 +8,8 @@ const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full' },
   {path: 'home', component: HomeComponent},
   {path: 'register-business', component: RegisterBusinessComponent},
-  {path: 'map-view', component: MapViewComponent}
+  {path: 'map-view', component: MapViewComponent},
+  {path: 'home/register-business', component: RegisterBusinessComponent}
 ];
 
 @NgModule({
