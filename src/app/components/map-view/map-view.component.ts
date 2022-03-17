4b7038d484786615a252f4e13bd6a4d9a8ed0d09
2711-bharath <2711bharath@gmail.com>
2022-03-17 11:08:52 +0530

    feat: added map-view filters UI

--- src/app/components/map-view/map-view.component.ts
+++ src/app/components/map-view/map-view.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import * as mapboxgl from 'mapbox-gl';
-import MapboxDraw from "@mapbox/mapbox-gl-draw";
 import { environment } from '../../../environments/environment';
 
 @Component({
@@ -10,41 +9,75 @@ import { environment } from '../../../environments/environment';
 })
 export class MapViewComponent implements OnInit {
   map: mapboxgl.Map;
+  businessTypes = [
+    {
+      value:"Education",
+      show: false,
+      icon: `<i class="fa-solid fa-school"></i>`
+    },
+    {
+      value:"Hospital",
+      show: false,
+      icon: `<i class="fa-solid fa-hospital"></i>`
+    },
+    {
+      value:"Residential",
+      show: false,
+      icon: `<i class="fas fa-building"></i>`
+    },
+    {
+      value:"Restaurant",
+      show: false,
+      icon: `<i class="fa-solid fa-utensils"></i>`
+    },
+    {
+      value:"Bus Stop",
+      show: false,
+      icon: `<i class="fa-solid fa-bus"></i>`
+    },
+    {
+      value:"Hotels",
+      show: false,
+      icon: `<i class="fa-solid fa-hotel"></i>`
+    },
+    {
+      value:"Cinema Theater",
+      show: false,
+      icon: `<i class="fas fa-film"></i>`
+    },
+    {
+      value:"Railway Station",
+      show: false,
+      icon: `<i class="fa-solid fa-subway"></i>`
+    },
+  ]
 
   ngOnInit(): void {
     mapboxgl as typeof mapboxgl;
     this.map = new mapboxgl.Map({
       accessToken: environment.mapbox.accessToken,
-      container: 'map',
+      container: 'map-view',
       style: 'mapbox://styles/mapbox/streets-v11', // style URL
       center: [-74.5, 40], // starting position [lng, lat]
       zoom: 9, // starting zoom
     });
 
-    // Add map controls
-    this.map.addControl(new mapboxgl.NavigationControl());
     this.map.addControl(
       new mapboxgl.GeolocateControl({
         positionOptions: {
-          enableHighAccuracy: true,
+          enableHighAccuracy: true
         },
         trackUserLocation: true,
-        showUserHeading: true,
-      })
+        showUserHeading: true
+      }), 'top-left'
     );
-    
-    const draw = new MapboxDraw({
-      displayControlsDefault: false,
-      controls: {
-        line_string: true,
-        rectangle: true,
-        point: true,
-        polygon: true,
-        trash: true,
-      },
-    });
-    console.log(draw)
-    this.map.addControl(draw, "top-right");
   }
+
+  onResize(event): void {
+    const map_view = document.getElementById("map-view")
+    map_view.style.height = "250px"
+    map_view.style.width = `width: ${event.target.innerWidth}px`
+  }
+
 
 }
